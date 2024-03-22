857fff5d90572d12a0fcda5958efa501b967e135
kwangyeon1 <qkqxldthfk1@gmail.com>
2024-03-23 02:49:34 +0900

    누락된 코드변경 내용 적용

--- src/main/java/api/freelive/board/adapter/out/persistence/adapter/PostJpaMapper.java
+++ src/main/java/api/freelive/board/adapter/out/persistence/adapter/PostJpaMapper.java
@@ -42,10 +42,4 @@ public class PostJpaMapper {
         );
     }
 
-
-    public static PostFileJpa toEntity(PostFile postFile) {
-        return PostFileJpa.builder()
-                .url(postFile.getUrl())
-                .build();
-    }
 }
